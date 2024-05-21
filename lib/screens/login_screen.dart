591eb35b5d923b45ca5110d7625655f0ad2e7161
Lars Anton <lars_anton@icloud.com>
2024-05-21 17:52:52 +0200

    Feature: Initial Structure for the Tabs Sign-In and Register are Implemented

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,22 +2,27 @@ import 'package:flutter/material.dart';
 import '../widgets/sign_in_form.dart';
 import '../widgets/register_form.dart';
 
+// LoginScreen Widget, das die Hauptanmeldeseite darstellt.
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStateMixin {
-  late TabController _tabController;
+  late TabController _tabController; // TabController zur Verwaltung der Tabs.
+  final Color indicatorColor = Color(0xFF252E3C); // Farbe des Indikators.
+  final Color backgroundColor = Color(0xFF44556F);// Farbe des Main-Hintergrunds.
 
   @override
   void initState() {
     super.initState();
+    // Initialisierung des TabControllers mit zwei Tabs.
     _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
   void dispose() {
+    // Freigabe der Ressourcen des TabControllers.
     _tabController.dispose();
     super.dispose();
   }
@@ -25,46 +30,66 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      // Hintergrundfarbe des Bildschirms.
+      backgroundColor: backgroundColor,
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(0), // Keine zusätzliche Auffüllung.
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center, // Zentriert den Inhalt vertikal.
           children: <Widget>[
+
+            // Platzhalter, um den Text etwas nach unten zu verschieben.
+            SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+
+            // Begrüßungstext.
             Text(
               'Welcome to PortfolioPilot',
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 24,
+                fontSize: 50,
               ),
+              textAlign: TextAlign.center,
             ),
+
+            // Abstand zwischen Text und TabBar.
             SizedBox(height: 20),
+
+            //Container für das Design der beiden Tabs.
             Container(
               decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(10),
+                color: Colors.white.withOpacity(0.1), // Hintergrundfarbe der TabBar mit Transparenz.
               ),
               child: TabBar(
-                controller: _tabController,
-                labelColor: Colors.white,
-                unselectedLabelColor: Colors.white54,
+                controller: _tabController, // Zuweisung des TabControllers.
+                labelColor: Colors.white, // Textfarbe der ausgewählten Tabs.
+                unselectedLabelColor: Colors.white54, // Textfarbe der nicht ausgewählten Tabs.
                 indicator: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.blue,
+                  color: indicatorColor, // Hintergrundfarbe des Indikators.
                 ),
+                indicatorSize: TabBarIndicatorSize.tab, // Indikatorgröße an Tabgröße anpassen.
                 tabs: [
-                  Tab(text: 'Sign-In'),
-                  Tab(text: 'Register'),
+                  Tab(text: 'Sign-In'), // Tab für die Anmeldung.
+                  Tab(text: 'Register'), // Tab für die Registrierung.
                 ],
               ),
             ),
-            SizedBox(height: 20),
+
+            // Füge eine farbige Linie unter der TabBar hinzu als "Sized Box".
+            Container(
+              height: 10,
+              color: indicatorColor,
+            ),
+
+            //Tabs und Widgets werden zugewiesen.
             Expanded(
+              // TabBarView zeigt den Inhalt der ausgewählten Tabs an.
               child: TabBarView(
-                controller: _tabController,
+                controller: _tabController, // Zuweisung des TabControllers.
                 children: [
-                  SignInForm(),
-                  RegisterForm(),
+                  // Übergebe die Hintergrundfarbe an das SignInForm-Widget.
+                  SignInForm(backgroundColor: indicatorColor),
+                  // Übergebe die Hintergrundfarbe an das RegisterForm-Widget.
+                  RegisterForm(backgroundColor: indicatorColor),
                 ],
               ),
             ),
--- lib/widgets/register_form.dart
+++ lib/widgets/register_form.dart
@@ -1,59 +1,96 @@
 import 'package:flutter/material.dart';
 
+// RegisterForm Widget, das ein Registrierungsformular darstellt.
 class RegisterForm extends StatelessWidget {
+  final Color backgroundColor;
+
+  RegisterForm({required this.backgroundColor});
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white.withOpacity(0.1),
-            hintText: 'Nutzername...',
-            prefixIcon: Icon(Icons.person, color: Colors.white),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
+    return Container(
+      // Hintergrundfarbe des Containers auf die übergebene Farbe gesetzt.
+      color: backgroundColor,
+      child: SingleChildScrollView(
+        // ScrollView, um sicherzustellen, dass der Inhalt gescrollt werden kann, wenn die Tastatur geöffnet wird.
+        child: Column(
+          children: <Widget>[
+
+            // Textfeld für den Benutzernamen.
+            TextFormField(
+              decoration: InputDecoration(
+                // Füllt das Textfeld mit einer halbtransparenten Farbe.
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.1),
+                hintText: 'Nutzername...', // Platzhaltertext.
+                prefixIcon: Icon(Icons.person, color: Colors.white), // Symbol links im Textfeld.
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none, // Kein Rahmen für das Textfeld.
+                ),
+                contentPadding: EdgeInsets.all(16.0), // Innenabstand im Textfeld.
+              ),
+              style: TextStyle(color: Colors.white), // Textfarbe im Textfeld.
             ),
-          ),
-          style: TextStyle(color: Colors.white),
-        ),
-        SizedBox(height: 10),
-        TextField(
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white.withOpacity(0.1),
-            hintText: 'Passwort...',
-            prefixIcon: Icon(Icons.visibility, color: Colors.white),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
+
+            // Abstand zwischen den Textfeldern.
+            SizedBox(height: 10),
+
+            // Textfeld für das Passwort.
+            TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.1),
+                hintText: 'Passwort...', // Platzhaltertext.
+                prefixIcon: Icon(Icons.visibility, color: Colors.white), // Symbol links im Textfeld.
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none, // Kein Rahmen für das Textfeld.
+                ),
+                contentPadding: EdgeInsets.all(16.0), // Innenabstand im Textfeld.
+              ),
+              obscureText: true, // Versteckt das eingegebene Passwort.
+              style: TextStyle(color: Colors.white), // Textfarbe im Textfeld.
             ),
-          ),
-          obscureText: true,
-          style: TextStyle(color: Colors.white),
-        ),
-        SizedBox(height: 10),
-        TextField(
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white.withOpacity(0.1),
-            hintText: 'Passwort bestätigen!',
-            prefixIcon: Icon(Icons.lock, color: Colors.white),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
+
+            // Abstand zwischen dem letzten Textfeld und dem Button.
+            SizedBox(height: 10),
+
+            // Textfeld zur Bestätigung des Passworts.
+            TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.1),
+                hintText: 'Passwort bestätigen!', // Platzhaltertext.
+                prefixIcon: Icon(Icons.lock, color: Colors.white), // Symbol links im Textfeld.
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none, // Kein Rahmen für das Textfeld.
+                ),
+                contentPadding: EdgeInsets.all(16.0), // Innenabstand im Textfeld.
+              ),
+              obscureText: true, // Versteckt das eingegebene Passwort.
+              style: TextStyle(color: Colors.white), // Textfarbe im Textfeld.
             ),
-          ),
-          obscureText: true,
-          style: TextStyle(color: Colors.white),
+
+            // Abstand zwischen dem letzten Textfeld und dem Button.
+            SizedBox(height: 10),
+
+            // Button zum Registrieren.
+            SizedBox(
+              width: double.infinity, // Button nimmt die gesamte Breite des Containers ein.
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal, // Hintergrundfarbe des Buttons
+                  foregroundColor: Colors.white, // Textfarbe des Buttons
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero, // Ecken nicht abrunden
+                  ),
+                ),
+                child: Text('Register'), // Text im Button.
+              ),
+            ),
+          ],
         ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {},
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.teal),
-          ),
-          child: Text('Register Now'),
-        ),
-      ],
+      ),
     );
   }
 }
