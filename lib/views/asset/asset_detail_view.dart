6daccc12954c7d96392c9ecb0cddcc6f8356c936
Lars Anton <lars_anton@icloud.com>
2024-06-30 22:08:15 +0200

    feature: Informationlists added for AssetsTypes

--- lib/views/asset/asset_detail_view.dart
+++ lib/views/asset/asset_detail_view.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:androidproject/utils/app_theme.dart';
 import 'package:androidproject/views/shared_widgets/chart_section.dart';
-import 'package:androidproject/views/asset/asset_widget/position_list.dart';
 import 'package:androidproject/views/asset/asset_widget/detailed_information_section.dart';
 import 'package:androidproject/views/asset/add_asset_widget/asset_detail_view_add.dart';
+import 'package:androidproject/views/asset/asset_widget/crypto_information_list.dart';
+import 'dart:convert';
+// import 'package:http/http.dart' as http;
 
 class AssetDetailView extends StatefulWidget {
   final String title;
-  final String currentPrice; // Neuer Parameter für den aktuellen Kurs
-  final String quantity; // Neuer Parameter für die Menge
-  final String purchasePrice; // Neuer Parameter für den Kaufpreis
-  final bool isPositive; // Neuer Parameter für den positiven Wert
+  final String currentPrice;
+  final String quantity;
+  final String purchasePrice;
+  final bool isPositive;
 
   const AssetDetailView({
     super.key,
@@ -19,7 +21,6 @@ class AssetDetailView extends StatefulWidget {
     required this.quantity,
     required this.purchasePrice,
     required this.isPositive,
-    required String amount,
   });
 
   @override
@@ -27,6 +28,97 @@ class AssetDetailView extends StatefulWidget {
 }
 
 class _AssetDetailViewState extends State<AssetDetailView> {
+  Map<String, dynamic> cryptoData = {};
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    loadTestData(); // Lade Testdaten für Entwicklung und Testzwecke
+  }
+
+  Future<void> loadTestData() async {
+    await Future.delayed(const Duration(seconds: 1));
+    setState(() {
+      cryptoData = {
+        "name": "Bitcoin",
+        "longName": "Bitcoin USD",
+        "previousClose": 60888.445,
+        "marketCap": 1216596672512,
+        "circulatingSupply": 19718732,
+        "volume24Hr": 13996477440,
+        "dayHigh": 61706.465,
+        "dayLow": 60646.215,
+        "fiftyTwoWeekHigh": 73750.07,
+        "fiftyTwoWeekLow": 24930.297,
+        "fiftyDayAverage": 66376.88,
+        "twoHundredDayAverage": 58015.203,
+        "averageVolume10days": 22655481892,
+        "regularMarketVolume": 13996477440,
+        "description": "Bitcoin (BTC) is a cryptocurrency launched in 2010. Users are able to generate BTC through the process of mining. Bitcoin has a current supply of 19,718,650. The last known price of Bitcoin is 61,442.46555255 USD and is up 0.97 over the last 24 hours. It is currently trading on 11335 active market(s) with 13,321,545,748.80 traded over the last 24 hours. More information can be found at https://bitcoin.org/.",
+      };
+      isLoading = false;
+    });
+  }
+  /*
+  Future<void> fetchCryptoData() async {
+  final url = 'https://example.com/api/crypto-data?title=${widget.title}'; // Beispielhafte API-URL
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    setState(() {
+      cryptoData = data['response'];
+      isLoading = false;
+    });
+  } else {
+    throw Exception('Failed to load data');
+  }
+}
+*/
+  /*
+  Future<void> fetchStockData() async {
+    final url = 'https://example.com/api/stock-data?title=${widget.title}'; // Beispielhafte API-URL
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        stockData = data['response'];
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  Future<void> fetchHoldings() async {
+    final url = 'https://example.com/api/etf-data?title=${widget.title}';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        holdings = data['response']['etfData']['fund_holding_info']['holdings'];
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  Future<void> fetchData() async {
+    final url = 'https://example.com/api/crypto-data?title=${widget.title}';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        cryptoData = data['response'];
+        isLoading = false;
+      });
+    } else {
+      await loadTestData(); // Lade Testdaten im Fehlerfall
+    }
+  }
+  */
+
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
@@ -69,7 +161,7 @@ class _AssetDetailViewState extends State<AssetDetailView> {
             ),
             child: Column(
               children: [
-                const SizedBox(height: kToolbarHeight - 20), // Platz für die AppBar
+                const SizedBox(height: kToolbarHeight - 20),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
@@ -82,7 +174,9 @@ class _AssetDetailViewState extends State<AssetDetailView> {
             ),
           ),
           Expanded(
-            child: ListView(
+            child: isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : ListView(
               padding: const EdgeInsets.all(16.0),
               children: [
                 Container(
@@ -125,8 +219,23 @@ class _AssetDetailViewState extends State<AssetDetailView> {
                   ),
                 ),
                 const SizedBox(height: 10),
-                const SizedBox(height: 10),
-                PositionList(),
+                CryptoInformationList(
+                  name: cryptoData['name'] ?? '',
+                  longName: cryptoData['longName'] ?? '',
+                  previousClose: cryptoData['previousClose'] ?? 0.0,
+                  marketCap: cryptoData['marketCap'] ?? 0.0,
+                  circulatingSupply: cryptoData['circulatingSupply'] ?? 0.0,
+                  volume24Hr: cryptoData['volume24Hr'] ?? 0.0,
+                  dayHigh: cryptoData['dayHigh'] ?? 0.0,
+                  dayLow: cryptoData['dayLow'] ?? 0.0,
+                  fiftyTwoWeekHigh: cryptoData['fiftyTwoWeekHigh'] ?? 0.0,
+                  fiftyTwoWeekLow: cryptoData['fiftyTwoWeekLow'] ?? 0.0,
+                  fiftyDayAverage: cryptoData['fiftyDayAverage'] ?? 0.0,
+                  twoHundredDayAverage: cryptoData['twoHundredDayAverage'] ?? 0.0,
+                  averageVolume10days: cryptoData['averageVolume10days'] ?? 0.0,
+                  regularMarketVolume: cryptoData['regularMarketVolume'] ?? 0.0,
+                  description: cryptoData['description'] ?? '',
+                ),
               ],
             ),
           ),
